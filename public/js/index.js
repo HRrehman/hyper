5852915f9dd03480f92fd185540e47de4ed44a22
Shahab Ali <code.door03@gmail.com>
2022-09-19 12:26:20 +0500

    countdown

--- public/js/index.js
+++ public/js/index.js
@@ -18,41 +18,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// counter 
+// countdown
 let countDownItem = Array.from(document.querySelectorAll('.count_down'))
-var timeInSecs;
-var ticker;
-
-function startTimer(secs) {
-  timeInSecs = parseInt(secs);
-  ticker = setInterval("tick()", 1000);
-}
-
-function tick() {
-  var secs = timeInSecs;
-  if (secs > 0) {
-    timeInSecs--;
+setInterval(function time() {
+  var d = new Date();
+  var days = 5 - d.getDay();
+  var hours = 24 - d.getHours();
+  var min = 60 - d.getMinutes();
+  if ((min + '').length == 1) {
+    min = '0' + min;
   }
-  else {
-    clearInterval(ticker);
-    startTimer(86400 * 5, 3600 * 5, 60 * 5);
+  var sec = 60 - d.getSeconds();
+  if ((sec + '').length == 1) {
+    sec = '0' + sec;
   }
-
-  var days = Math.floor(secs / 86400);
-  secs %= 86400;
-  var hours = Math.floor(secs / 3600);
-  secs %= 3600;
-  var mins = Math.floor(secs / 60);
-  secs %= 60;
-  var day = ((days < 10) ? "0" : "") + days;
-  var hour = ((hours < 10) ? "0" : "") + hours;
-  var min = ((mins < 10) ? "0" : "") + mins;
-  var sec = ((secs < 10) ? "0" : "") + secs
-  countDownItem[0].textContent = day
-  countDownItem[1].textContent = hour
+  countDownItem[0].textContent = days
+  countDownItem[1].textContent = hours
   countDownItem[2].textContent = min
   countDownItem[3].textContent = sec
-}
-
-startTimer(86400 * 5, 3600 * 5, 60 * 5);
-
+}, 1000);
